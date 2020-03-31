2c8824a80d896c1d3aad0b7e96535a8c11d6ef4d
fayyazarshad <fayyazarshad@folio3.com>
2020-03-31 18:24:32 +0500

    convert all entry points of js

---
+++ packages/optimizely-sdk/scripts/build.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2020, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const fs = require("fs");
+const path = require("path");
+const execSync = require("child_process").execSync;
+
+process.chdir(path.resolve(__dirname, ".."));
+
+function exec(command, extraEnv) {
+  return execSync(command, {
+    stdio: "inherit",
+    env: Object.assign({}, process.env, extraEnv)
+  });
+}
+
+const umdName = "optimizelySdk";
+
+console.log("\nBuilding CommonJS modules...");
+exec(`rollup -c scripts/config.js -i lib/index.node.js -f cjs -o dist/optimizely.node.js`);
+
+exec(`rollup -c scripts/config.js -i lib/index.browser.js -f cjs -o dist/optimizely.browser.js`);
+
+exec(`rollup -c scripts/config.js -i lib/index.react_native.js -f cjs -o dist/optimizely.react_native.js`);
---
+++ packages/optimizely-sdk/scripts/config.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2020, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+const packageDeps = require('../package.json').dependencies || {};
+
+function getExternals() {
+  return ['https', 'http', 'url'].concat(Object.keys(packageDeps));
+}
+
+function getPlugins (){
+  const plugins = [
+    resolve({
+      browser: true,
+    }),
+    commonjs({
+      namedExports:
+        {
+          '@optimizely/js-sdk-logging': [
+            'getLogger',
+            'setLogLevel',
+            'LogLevel',
+            'setLogHandler',
+            'setErrorHandler',
+            'getErrorHandler'
+          ],
+          '@optimizely/js-sdk-event-processor': [
+            'LocalStoragePendingEventsDispatcher'
+          ]
+        },
+    }),
+  ];
+
+  return plugins;
+}
+
+module.exports = {
+  plugins: getPlugins(),
+  external: getExternals(),
+  output: {
+    globals: {
+      '@optimizely/js-sdk-logging': 'logging',
+      '@optimizely/js-sdk-event-processor': 'eventProcessor',
+      '@optimizely/js-sdk-datafile-manager': 'datafileManager',
+      '@optimizely/js-sdk-utils': 'jsSdkUtils',
+      murmurhash: 'murmurhash',
+      uuid: 'v4',
+    },
+    exports: 'named',
+  }
+}
