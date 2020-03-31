4747b0d3b33abe778dd0abb0fb6d4a099d9d8bab
Jason Karns <jason+github@testdouble.com>
2020-03-31 17:35:26 -0400

    Add commonjs and node-resolve rollup plugins

--- packages/optimizely-sdk/rollup.config.js
+++ packages/optimizely-sdk/rollup.config.js
@@ -1,7 +1,14 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
 export default {
   input: 'lib/index.browser.js',
   output: {
     file: 'dist/optimizely.github.js',
     format: 'es'
-  }
+  },
+  plugins: [
+    resolve(),
+    commonjs()
+  ]
 }
