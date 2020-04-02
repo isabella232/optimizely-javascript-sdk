4f5a1da3c4e670928818d34c49cbc4bd78352f28
fayyazarshad <fayyazarshad@folio3.com>
2020-04-02 14:32:05 +0500

    testing umd bundle from rollup

--- packages/optimizely-sdk/rollup.config.js
+++ packages/optimizely-sdk/rollup.config.js
@@ -13,26 +13,82 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from  'rollup-plugin-terser'
 import { dependencies } from './package.json';
 
-function getConfigForPlatform(platform) {
+const getConfigForPlatform = (platform) => {
   return {
-    plugins: [resolve(), commonjs(), terser()],
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser()
+    ],
     external: ['https', 'http', 'url'].concat(Object.keys(dependencies || {})),
-    input: 'lib/index.' + platform + '.js',
+    input: `lib/index.${platform}.js`,
     output: {
       exports: 'named',
       format: 'cjs',
-      file: 'dist/optimizely.' + platform + '.min.js',
+      file: `dist/optimizely.${platform}.min.js`,
     }
   };
+};
+
+const namedExports = {
+  '@optimizely/js-sdk-logging': [
+    'getLogger',
+    'setLogLevel',
+    'LogLevel',
+    'setLogHandler',
+    'setErrorHandler',
+    'getErrorHandler'
+  ],
+  '@optimizely/js-sdk-event-processor': [
+    'LocalStoragePendingEventsDispatcher'
+  ]
+};
+
+const globals = {
+  '@optimizely/js-sdk-logging': 'logging',
+  '@optimizely/js-sdk-event-processor': 'eventProcessor',
+  '@optimizely/js-sdk-datafile-manager': 'datafileManager',
+  '@optimizely/js-sdk-utils': 'jsSdkUtils',
+  murmurhash: 'murmurhash',
+  uuid: 'v4',
+};
+
+const getPlugins = (env) => {
+  const plugins = [
+    resolve({ browser: true }),
+    commonjs({ namedExports: namedExports }),
+  ];
+
+  if (env === 'production') {
+    plugins.push(terser());
+  }
+  return plugins;
 }
+
+const getConfigForUMD = (env) => {
+  return {
+    plugins: getPlugins(env),
+    external: ['https', 'http', 'url'].concat(Object.keys(dependencies || {})),
+    input: 'lib/index.browser.js',
+    output: {
+      name: 'optimizelySdk',
+      format: 'umd',
+      file: `dist/optimizely.browser.umd${ env === 'production' ? '.min' : '' }.js`,
+      exports: 'named',
+      globals: globals,
+    },
+  }
+};
 
 export default [
   getConfigForPlatform('node'),
   getConfigForPlatform('browser'),
   getConfigForPlatform('react_native'),
+  getConfigForUMD('development'),
+  getConfigForUMD('production'),
 ]
