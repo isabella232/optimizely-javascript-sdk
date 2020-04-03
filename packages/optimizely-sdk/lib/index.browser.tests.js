6f725c6f8ab1f5abbe08a13326992047c3230da6
fayyazarshad <fayyazarshad@folio3.com>
2020-04-03 17:59:23 +0500

    update karma config

--- packages/optimizely-sdk/lib/index.browser.tests.js
+++ packages/optimizely-sdk/lib/index.browser.tests.js
@@ -54,7 +54,7 @@ describe('javascript-sdk', function() {
         sinon.stub(configValidator, 'validate');
 
         xhr = sinon.useFakeXMLHttpRequest();
-        window.XMLHttpRequest = xhr;
+        global.XMLHttpRequest = xhr;
         requests = [];
         xhr.onCreate = function(req) {
           requests.push(req);
